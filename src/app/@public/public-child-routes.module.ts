5b2f63e19b6a502e189af4495751aad5d900341e
Jorge Cervantes <jorgecdev@gmail.com>
2021-03-15 18:36:25 -0600

    lazyloading en la seccion publica

--- src/app/@public/public-child-routes.module.ts
+++ src/app/@public/public-child-routes.module.ts
@@ -1,10 +1,12 @@
+import { HomeComponent } from './home/home.component';
 import { PublicComponent } from './public.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const childRoutes: Routes = [
     // { path: '', component: HomeComponent },
-    { path: '', component: PublicComponent }
+    { path: '', component: PublicComponent },
+    { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
--- src/app/@public/public.module.ts
+++ src/app/@public/public.module.ts
@@ -1,3 +1,5 @@
+import { RouterModule } from '@angular/router';
+import { PublicChildRoutesModule } from './public-child-routes.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
@@ -5,7 +7,7 @@ import { PublicComponent } from './public.component';
 
 @NgModule({
     declarations: [HomeComponent, PublicComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     exports: [HomeComponent]
 })
 export class PublicModule {}
