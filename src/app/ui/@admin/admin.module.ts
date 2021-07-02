52c8cb3420a419812c9b3702e1e207b7fdf72cf0
Jorge Cervantes <jorgecdev@gmail.com>
2021-07-02 18:19:19 -0500

    admin component added

--- src/app/ui/@admin/admin.module.ts
+++ src/app/ui/@admin/admin.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { EntityComponent } from './pages/entity/entity.component';
+import { SharedModule } from '../shared/shared.module';
+import { AdminComponent } from './admin.component';
 
 @NgModule({
-    declarations: [DashboardComponent, EntityComponent],
-    imports: [CommonModule],
-    exports: [DashboardComponent]
+    declarations: [AdminComponent, DashboardComponent, EntityComponent],
+    imports: [CommonModule, SharedModule],
+    exports: []
 })
 export class AdminModule {}
