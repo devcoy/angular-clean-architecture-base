227d8dfa8cc2b0cd00ad7a27c50da2473e92f16e
Jorge Cervantes <jorgecdev@gmail.com>
2021-07-02 18:30:12 -0500

    admin navbar component created

--- src/app/ui/@admin/admin.module.ts
+++ src/app/ui/@admin/admin.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { EntityComponent } from './pages/entity/entity.component';
 import { SharedModule } from '../shared/shared.module';
 import { AdminComponent } from './admin.component';
+import { AdminNavbarComponent } from './components/admin-navbar/admin-navbar.component';
 
 @NgModule({
-    declarations: [AdminComponent, DashboardComponent, EntityComponent],
-    imports: [CommonModule, SharedModule],
+    declarations: [
+        AdminComponent,
+        DashboardComponent,
+        EntityComponent,
+        AdminNavbarComponent
+    ],
+    imports: [CommonModule, RouterModule, SharedModule],
     exports: []
 })
 export class AdminModule {}
